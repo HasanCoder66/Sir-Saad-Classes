c18235b58096113279e8b376d756e67da1f812b2
hasanCoder66 <hasanashraf874@gmail.com>
2026-03-02 09:36:01 +0500

    if else

---
+++ if-else-else-if/app.js
@@ -0,0 +1,107 @@
+// console.log("file is running -->")
+
+
+
+
+
+// if syntax 
+
+
+// keyword,  parentheses,  curly bracket
+            // condition , block of code 
+            // var purchaseItem = prompt("kia lana hai")
+
+            // if(purchaseItem == "mobile"){
+            //     console.log("kitny ka hai mobile")
+            // }else {
+            //     console.log("to wapis ajaou")
+            // }
+
+
+// if (name == "ahsan"){
+//     console.log(name)
+// }
+
+
+// let acRemote = "Yes"
+
+// if(acRemote == "Yes" ){
+//     console.log("Ac Open kerdo")
+// }else {
+//     console.log("Remote nhi hai....")
+// }
+
+
+
+// let age = 12;
+
+// if(age > 18){
+//     console.log("garri chlaaa looo")
+// }else {
+//     console.log("barry hojaou")
+// }
+
+
+
+// let city = "Lahore";
+
+// if(city  === "Karachi"){
+//     console.log("Welcome To City of Lights")
+// }else {
+//     console.log("Karachi Ajaou")
+// }
+
+
+// let userAns = "ok";
+// let correctAns = "ok";
+// let count = 0;
+
+
+// if(userAns == correctAns){
+//     count++
+// }
+
+// console.log(count)
+
+
+// let name;
+
+// name ="hasan"
+
+// name = "ahsan"
+
+
+// let count = 0;
+
+// count++
+
+
+
+// == data type check nhi kerta.  ==> equaly 
+// === data type check kerta hai..  ==> strict equal
+// ! false value ko true //  agar true hoi to false ==> not operator    
+
+// console.log(2 === "2")
+
+
+// let led = false
+// if(!led){
+//     console.log("Chalaa lo ==>")
+// }
+
+// if( 0){
+//     console.log(0)
+// }
+
+// let age = 19
+// if(age > 18){
+//     console.log("tm jaou or vote daaldo")
+// }
+
+
+let password = "1234567"
+
+console.log(password.length)
+if(password.length < 8){
+    console.log("Password at least 8 characters")
+}
