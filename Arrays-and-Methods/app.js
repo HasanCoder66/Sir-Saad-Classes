afea0fecf23d3879123f7248a8fc653084b4ad6f
hasanCoder66 <hasanashraf874@gmail.com>
2026-03-09 09:51:29 +0500

    Arrays and it's methods

---
+++ Arrays-and-Methods/app.js
@@ -0,0 +1,13 @@
+// console.log("js is running -->")
+
+
+// let animals = ["dog", "goat", "lion", "donkey","horse","cat"];
+
+// animals[2] = "frog"
+// console.log(animals[2])
+
+// let mixedArr = ["string", true, false, undefined, null, [[true],[false], null]]
+
+// console.log(mixedArr)
+
+
---
+++ Arrays/index.js
@@ -0,0 +1,12 @@
+// var city0 = "Karachi";
+var city1 ;
+console.log(city1)
+
+
+// var username = []
+
+// Arrays ==>  []
+
+    var cities = ["Karachi", "Lahore"]
+    console.log(cities[1])
+
