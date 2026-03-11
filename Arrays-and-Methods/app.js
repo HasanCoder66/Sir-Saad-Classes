6ddee86babc14e972155a30867bfdf50584ba807
hasanCoder66 <hasanashraf874@gmail.com>
2026-03-11 09:49:25 +0500

    array methods

--- Arrays-and-Methods/app.js
+++ Arrays-and-Methods/app.js
@@ -11,3 +11,31 @@
 // console.log(mixedArr)
 
 
+
+let arr = ["ahsan", "shakoor"];
+
+// console.log()
+// console.log("hasan")
+
+
+// let returnValue = arr.shift() // declare and initialize 
+
+// console.log(returnValue + " ashraf", "REmove Elemetn hai")
+
+
+let arrNum = [12, 56,22, 99, 3, 55, 2, 1 ]
+
+// arrNum.reverse()
+// console.log(arrNum)
+
+
+// copy kerta hai ==> array || string
+
+// return dega slice 1 array   == > [12, 56,22]
+// let row1 = arrNum.slice(5)
+// let row2 = arrNum.slice(4,9)
+// console.log(row1)
+// console.log(row2)
+
+// console.log(arrNum)
+
